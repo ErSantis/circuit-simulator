d2f5656a5472e8b222fab12626382caeca370e1f
Elkin <ersantis@uninorte.edu.co>
2022-10-16 22:48:59 -0500

    create server a get json

--- server.py
+++ server.py
@@ -1,22 +1,26 @@
-from re import M
+from solve import Circuit
 from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
 import json
+
 app = Flask(__name__, template_folder='templates')
+CORS(app)
 
+example1 = Circuit()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/test', methods=['POST'])
 def test():
-    output = request.get_json()
-    print(output) # This is the output that was stored in the JSON within the browser
-    print(type(output))
-    result = json.loads(output) #this converts the json output to a python dictionary
-    print(result) # Printing the new dictionary
-    print(type(result))#this shows the json converted as a python dictionary
-    return result
+    body = request.json
+
+    example1.add_branch((1, 2), R=body[0][1], V=body[0][3])
+    example1.add_branch((1, 2), R=body[1][1], V=body[1][3])
+    example1.add_branch((1, 2), R=body[2][1], V=body[2][3])
+
+    print(example1)
+    return jsonify(body)
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+    app.run(debug=True, port=5501)
