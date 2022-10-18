dd122d5d6719e3d86c7f6d4b7502e928aab080ff
Elkin <ersantis@uninorte.edu.co>
2022-10-18 16:02:22 -0500

    Server ready, casting to json and solve circuits succesfully

--- server.py
+++ server.py
@@ -14,15 +14,15 @@ CORS(app)
 def circuit1():
     
     body = request.json
-    print(body)
-    example1 = Circuit()
-    R1=body[0][1]
-    R2=body[1][1]
-    R3=body[2][1]
 
-    V1=body[0][3]
-    V2=body[1][3]
-    V3=body[2][3]
+    example1 = Circuit()
+    R1=float(body[0][1])
+    R2=float(body[1][1])
+    R3=float(body[2][1])
+
+    V1=float(body[0][3])
+    V2=float(body[1][3])
+    V3=float(body[2][3])
 
     example1.add_branch((1, 2), R=R1, V=V1)
     example1.add_branch((1, 2), R=R2, V=V2)
@@ -39,24 +39,68 @@ def circuit1():
 @app.route('/circuit2', methods=['POST'])
 def circuit2():
     body = request.json
-    R1=body[0][1]
-    V1=body[0][3]
-
-    R2=body[1][1]
-    R3=body[2][1]
+    
     example2 = Circuit()
+    R1=float(body[0][1])
+    V1=float(body[0][3])
+
+    R2=float(body[1][1])
+    R3=float(body[2][1])
+
     example2.add_branch((1, 2), R = R1, V = V1)
-    example2.add_branch((1, 2), R = R2, I = 5)
+    example2.add_branch((1, 2), R = R2, V = 0)
     example2.add_branch((1, 2), R = R3, V = 0)
-    print(example2)
-    return jsonify(body)
+    
+    intensidades = []
+    for branch in example2.branches:
+        print('I',abs(example2._circuit.edges[branch]['I']), 'R', example2._circuit.edges[branch]['R'], 'V', example2._circuit.edges[branch]['V'])
+        intensidades.append(float(abs(example2._circuit.edges[branch]['I'])))
+    print(intensidades)
+
+    return jsonify(intensidades)
 
 
 @app.route('/circuit3', methods=['POST'])
 def circuit3():
     body = request.json
-    print(body)
-    return jsonify(body)
+
+    example3 = Circuit()
+    R1=float(body[0][1])
+    V1=float(body[0][3])
+
+    R2=float(body[1][1])
+
+    R3=float(body[2][1])
+    V3=float(body[2][3])
+
+    R4=float(body[3][3])
+    V4=-1*float(body[3][1])
+
+    R5=float(body[4][1])
+    V5=float(body[4][3])
+
+    R6=float(body[5][1])+float(body[5][3])
+    R7=float(body[6][1])
+
+    R8=float(body[7][1])+float(body[7][3])
+    V8=float(body[7][7])-float(body[7][5])
+
+    example3.add_branch(('A', 'B'), R = R1, V = V1)
+    example3.add_branch(('A', 'C'), R = R2, V = 0)
+    example3.add_branch(('A', 'D'), R = R3, V = V3)
+    example3.add_branch(('B', 'C'), R = R4, V = V4)
+    example3.add_branch(('B', 'C'), R = R5, V = V5)
+    example3.add_branch(('B', 'D'), R = R6, V = 0)
+    example3.add_branch(('B', 'D'), R = R7, V = 0)
+    example3.add_branch(('A', 'B'), R = R8, V = V8)
+
+    intensidades = []
+    for branch in example3.branches:
+        print('I',abs(example3._circuit.edges[branch]['I']), 'R', example3._circuit.edges[branch]['R'], 'V', example3._circuit.edges[branch]['V'])
+        intensidades.append(float(abs(example3._circuit.edges[branch]['I'])))
+    print(intensidades)
+
+    return jsonify(intensidades)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5501)
